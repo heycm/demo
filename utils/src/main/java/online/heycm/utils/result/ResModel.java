f842cf530d15c61d53371993a0d9a461b3e4325b
heycm <heycm@qq.com>
2020-08-01 17:03:20 +0800

    2020-8-1 17:02:26 heycm 工具包构建统一返回对象

---
+++ utils/src/main/java/online/heycm/utils/result/ResModel.java
@@ -0,0 +1,9 @@
+package online.heycm.utils.result;
+
+/**
+ * @Description
+ * @Author heycm@qq.com
+ * @Date 2020-08-01 17:00
+ */
+public class ResModel {
+}
---
+++ utils/src/main/java/online/heycm/utils/result/Result.java
@@ -0,0 +1,9 @@
+package online.heycm.utils.result;
+
+/**
+ * @Description
+ * @Author heycm@qq.com
+ * @Date 2020-08-01 17:01
+ */
+public class Result {
+}
